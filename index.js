5239ff3b7fd7f66396212f41bb1fbafa035445b7
Dwayne <dune.njenga@student.moringaschool.com>
2025-03-05 15:34:47 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(streetNumber){
+    return Math.abs(42- streetNumber);
+}
+
+function distanceFromHqInFeet(streetNumber){
+    let blocks = distanceFromHqInBlocks(streetNumber);
+    return (blocks * 264 );
+}
+
+function distanceTravelledInFeet(pickupStreet, destinationStreet){
+    let blocks = Math.abs((destinationStreet - pickupStreet));
+    let feet = (blocks * 264);
+    return feet ;
+}
+
+function calculatesFarePrice(pickupStreet, destinationStreet){
+    let blocks = Math.abs((destinationStreet - pickupStreet));
+    let feet = (blocks * 264);
+    if (feet <= 400 ){
+        let fare = 0;
+        return fare;
+    }else if (feet <= 2000){
+        feet = feet - 400;
+        let fare = (feet * 0.02);
+        return fare;
+    }else if (feet <= 2500){
+        const fare = 25;
+        return fare;
+    }else {
+        return "cannot travel that far"
+    }
+}
+
+
+
